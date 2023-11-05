cb494bd7c1f87af1674308f448198ac37453c18b
Chrstn67 <chrstn.hmbrt67.com>
2023-11-05 10:52:27 +0100

    MAJ Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ function Header() {
   return (
     <header className="Header">
       <div>
-        <img src="Logo-site.jpg" alt="Logo du site PaleoData" />
+        <img src="PaleoData.png" alt="Logo du site PaleoData" />
         <h1>PaleoData</h1>
         <p>Un safari préhistorique à la rencontre d'animaux ayant peuplé notre Terre !</p>
       </div>
