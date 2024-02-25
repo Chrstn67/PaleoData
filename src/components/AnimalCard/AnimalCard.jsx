125639e31dff73355e79e9735a2065ad09fbf1d5
Chrstn67 <chrstn.hmbrt67.com>
2024-02-25 11:41:28 +0100

    MAJ de la disposition des éléments de AnimalCard

--- src/components/AnimalCard/AnimalCard.jsx
+++ src/components/AnimalCard/AnimalCard.jsx
@@ -46,146 +46,161 @@ const AnimalCard = ({ data }) => {
 
   return (
     <div className="animal-card">
-      <button
-        type="button"
-        onClick={() => {
-          shareLink(animal);
-        }}
-      >
-        <BiShareAlt size={20} />
-      </button>
-      <h2>{animal.nom}</h2>
-      <img src={imageUrl} alt={animal.nom} />
+      <section className="left-section">
+        <button
+          type="button"
+          onClick={() => {
+            shareLink(animal);
+          }}
+        >
+          <BiShareAlt size={20} />
+        </button>
+        <section className="animal-generalites">
+          <h2>{animal.nom}</h2>
+          <img src={imageUrl} alt={animal.nom} />
 
-      <div>
-        <h3>Etymologie</h3>
-        {animal.etymologie && <p>{animal.etymologie}</p>}
-      </div>
-
-      {animal.description && (
-        <div>
-          <h3>Description</h3>
-          <p>{animal.description}</p>
-        </div>
-      )}
-
-      {animal.regime_alimentaire && (
-        <div>
-          <h3>Régime alimentaire</h3>
-          <p>{animal.regime_alimentaire}</p>
-        </div>
-      )}
-
-      {animal.habitat && (
-        <div>
-          <h3>Habitat</h3>
-          <p>{animal.habitat}</p>
-        </div>
-      )}
-      {animal.geologie && (
-        <div>
-          <h3>Géologie</h3>
-          {animal.geologie.apparition && (
-            <p>
-              <span>Apparition:</span> {animal.geologie.apparition} Ma
-            </p>
-          )}
-          {animal.geologie.ere && (
-            <p>
-              <span>Ère:</span> {animal.geologie.ere}
-            </p>
-          )}
-          {animal.geologie.periode && (
-            <p>
-              <span>Période:</span> {animal.geologie.periode}
-            </p>
-          )}
-          {animal.geologie.epoque && (
-            <p>
-              <span>Époque:</span> {animal.geologie.epoque}
-            </p>
-          )}
-          {animal.geologie.etage && (
-            <p>
-              <span>Étage:</span> {animal.geologie.etage}
-            </p>
-          )}
-
-          {animal.geologie.extinction && (
-            <p>
-              <span>Extinction:</span> {animal.geologie.extinction} Ma
-            </p>
-          )}
-        </div>
-      )}
-      {animal.autres_infos && (
-        <div>
-          <h3>Autres informations</h3>
-          {animal.autres_infos.taille && (
-            <>
-              <p>
-                <span>Taille (Longueur): </span> <br />
-                {animal.autres_infos.taille.longueur}
-              </p>
-              {animal.autres_infos.taille.hauteur && (
+          <div>
+            <h3>Etymologie</h3>
+            {animal.etymologie && <p>{animal.etymologie}</p>}
+          </div>
+        </section>
+        <section className="animal-geologie">
+          {animal.geologie && (
+            <div>
+              <h3>Géologie</h3>
+              {animal.geologie.apparition && (
                 <p>
-                  <span>Taille (Hauteur): </span> <br />
-                  {animal.autres_infos.taille.hauteur}
+                  <span>Apparition:</span> {animal.geologie.apparition} Ma
                 </p>
               )}
-              {animal.autres_infos.taille.envergure && (
+              {animal.geologie.ere && (
                 <p>
-                  <span>Taille (Envergure): </span> <br />
-                  {animal.autres_infos.taille.envergure}
+                  <span>Ère:</span> {animal.geologie.ere}
                 </p>
               )}
-            </>
-          )}
-          {animal.autres_infos.poids && (
-            <p>
-              <span>Poids:</span> <br />
-              {animal.autres_infos.poids}
-            </p>
-          )}
-        </div>
-      )}
-      {animal.decouverte && (
-        <div>
-          <h3>Découverte</h3>
-          {animal.decouverte.date && (
-            <p>
-              <span>Date:</span> {animal.decouverte.date}
-            </p>
-          )}
-          {animal.decouverte.lieu && (
-            <p>
-              <span>Lieu: </span>
-              {animal.decouverte.lieu}
-            </p>
-          )}
-        </div>
-      )}
-
-      {Object.entries(animal.taxonomie).length > 0 && (
-        <div className="taxon">
-          <h3>Taxonomie</h3>
-          <table>
-            <tbody>
-              {Object.entries(animal.taxonomie).map(
-                ([key, value]) =>
-                  value && (
-                    <tr key={key}>
-                      <td>
-                        <span>{formatTaxonName(key)}:</span>
-                      </td>
-                      <td>{value}</td>
-                    </tr>
-                  ),
+              {animal.geologie.periode && (
+                <p>
+                  <span>Période:</span> {animal.geologie.periode}
+                </p>
               )}
-            </tbody>
-          </table>
-        </div>
-      )}
+              {animal.geologie.epoque && (
+                <p>
+                  <span>Époque:</span> {animal.geologie.epoque}
+                </p>
+              )}
+              {animal.geologie.etage && (
+                <p>
+                  <span>Étage:</span> {animal.geologie.etage}
+                </p>
+              )}
+
+              {animal.geologie.extinction && (
+                <p>
+                  <span>Extinction:</span> {animal.geologie.extinction} Ma
+                </p>
+              )}
+            </div>
+          )}
+        </section>
+      </section>
+
+      <section className="right-section">
+        <section className="animal-description">
+          {animal.description && (
+            <div>
+              <h3>Description</h3>
+              <p>{animal.description}</p>
+            </div>
+          )}
+
+          {animal.regime_alimentaire && (
+            <div>
+              <h3>Régime alimentaire</h3>
+              <p>{animal.regime_alimentaire}</p>
+            </div>
+          )}
+
+          {animal.habitat && (
+            <div>
+              <h3>Habitat</h3>
+              <p>{animal.habitat}</p>
+            </div>
+          )}
+        </section>
+
+        <section className="animal-autresInfos">
+          {animal.autres_infos && (
+            <div>
+              <h3>Autres informations</h3>
+              {animal.autres_infos.taille && (
+                <>
+                  <p>
+                    <span>Taille (Longueur): </span> <br />
+                    {animal.autres_infos.taille.longueur}
+                  </p>
+                  {animal.autres_infos.taille.hauteur && (
+                    <p>
+                      <span>Taille (Hauteur): </span> <br />
+                      {animal.autres_infos.taille.hauteur}
+                    </p>
+                  )}
+                  {animal.autres_infos.taille.envergure && (
+                    <p>
+                      <span>Taille (Envergure): </span> <br />
+                      {animal.autres_infos.taille.envergure}
+                    </p>
+                  )}
+                </>
+              )}
+              {animal.autres_infos.poids && (
+                <p>
+                  <span>Poids:</span> <br />
+                  {animal.autres_infos.poids}
+                </p>
+              )}
+            </div>
+          )}
+          {animal.decouverte && (
+            <div>
+              <h3>Découverte</h3>
+              {animal.decouverte.date && (
+                <p>
+                  <span>Date:</span> {animal.decouverte.date}
+                </p>
+              )}
+              {animal.decouverte.lieu && (
+                <p>
+                  <span>Lieu: </span>
+                  {animal.decouverte.lieu}
+                </p>
+              )}
+            </div>
+          )}
+        </section>
+      </section>
+
+      <section className="bottom-section">
+        {Object.entries(animal.taxonomie).length > 0 && (
+          <section className="taxonomy">
+            <h3>Taxonomie</h3>
+            <table>
+              <tbody>
+                {Object.entries(animal.taxonomie).map(
+                  ([key, value]) =>
+                    value && (
+                      <tr key={key}>
+                        <td>
+                          <span>{formatTaxonName(key)}:</span>
+                        </td>
+                        <td>{value}</td>
+                      </tr>
+                    ),
+                )}
+              </tbody>
+            </table>
+          </section>
+        )}
+      </section>
     </div>
   );
 };
