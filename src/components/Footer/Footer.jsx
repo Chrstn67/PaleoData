a12cadae5dfd3ef54de380f75f433c04a9394e8b
Chrstn67 <chrstn.hmbrt67.com>
2024-04-02 12:59:28 +0200

    changement de sémantique du footer et des information contenue dans ce footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,38 +8,8 @@ const Footer = () => {
 
   return (
     <footer className="footer">
-      <div className="footer-content">
-        <div className="footer-section">
-          <h3 className="footer-title">Liens</h3>
-          <ul className="footer-links">
-            <li>
-              <Link className="footer-link" to="/">
-                Accueil
-              </Link>
-            </li>
-            <li>
-              <Link className="footer-link" to="/animaux">
-                Animaux
-              </Link>
-            </li>
-            <li>
-              <Link className="footer-link" to="/echelle-des-temps-geologiques">
-                Échelle des temps
-              </Link>
-            </li>
-            <li>
-              <Link className="footer-link" to="/liste-etymologique">
-                Etymologie
-              </Link>
-            </li>
-            <li>
-              <Link className="footer-link" to="/documentation">
-                Documentation
-              </Link>
-            </li>
-          </ul>
-        </div>
-        <div className="footer-section">
+      <section className="footer-content">
+        <section className="footer-section">
           <h3 className="footer-title">Contactez-nous</h3>
           <Link to="https://discord.gg/fT97dr49Tv" target="_blank" rel="noopener noreferrer" title="Discord">
             <FaDiscord className="footer-discord-icon" />
@@ -47,16 +17,16 @@ const Footer = () => {
           <Link href="mailto:paleodata@outlook.com" target="_blank" rel="noopener noreferrer" title="Mail">
             <FaEnvelope className="footer-email-icon" />
           </Link>
-        </div>
-        <div className="footer-section">
+        </section>
+        <section className="footer-section">
           <h3 className="footer-title">À propos</h3>
           <p className="footer-text">
             PaleoData est votre porte d'entrée dans le monde de la vie ancienne. Notre mission est de fournir un accès
             aux données et aux informations paléontologiques aux données et informations paléontologiques de manière
             ouverte et conviviale.
           </p>
-        </div>
-        <div className="footer-section">
+        </section>
+        <section className="footer-section">
           <img src="Logo.jpg" alt="Logo développeur" className="footer-logo" />
           <p className="footer-copyright">
             &copy; {currentYear} PaleoData. Tous droits réservés. <br />{' '}
@@ -64,8 +34,8 @@ const Footer = () => {
               Mentions légales
             </Link>
           </p>
-        </div>
-      </div>
+        </section>
+      </section>
     </footer>
   );
 };
