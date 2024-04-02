0896493b77116204f735d09334421fec03b98398
Chrstn67 <chrstn.hmbrt67.com>
2024-04-02 16:29:01 +0200

    travail sur une meilleur gestion de la navbar en format mobile (brouillon)

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,19 @@
-import NavBar from './NavBar/NavBar';
+import NavBar from '../Header/NavBar/NavBar';
 import './Header.scss';
 
 function Header() {
   return (
-    <header className="Header">
-      <div>
-        <img src="PaleoData.png" alt="Logo du site PaleoData" />
-        <h1>PaleoData</h1>
-        <p>Safari préhistorique à la rencontre d'animaux ayant peuplé notre Terre !</p>
-      </div>
+    <>
+      <header className="Header">
+        <div>
+          <img src="PaleoData.png" alt="Logo du site PaleoData" />
+          <h1>PaleoData</h1>
+          <p>Safari préhistorique à la rencontre d'animaux ayant peuplé notre Terre !</p>
+        </div>
+      </header>
+
       <NavBar />
-    </header>
+    </>
   );
 }
 
