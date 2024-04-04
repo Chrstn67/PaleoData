1ef01b7b5f356846cadd13e56b91a63d1f6d896d
Chrstn67 <chrstn.hmbrt67.com>
2024-04-04 17:58:35 +0200

    mise en place de la timeline (react-vertical-timeline-component + react-device-detect + react-modal).

--- src/components/Timeline/Timeline.jsx
+++ src/components/Timeline/Timeline.jsx
@@ -1,24 +1,27 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import ModalTimeline from './ModalTimeline/ModalTimeline';
+import 'react-vertical-timeline-component/style.min.css';
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
+import { isMobile } from 'react-device-detect';
+import Modal from 'react-modal';
+
 import './Timeline.scss';
 
 const Timeline = ({ timelineData }) => {
   const [openEra, setOpenEra] = useState(null);
   const [openPeriod, setOpenPeriod] = useState(null);
   const [openEpoch, setOpenEpoch] = useState(null);
-
+  const [showMore, setShowMore] = useState({});
+  const [activeInfo, setActiveInfo] = useState(null);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
   const [modalContent, setModalContent] = useState(null);
-  const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const handleInfoClick = (content) => {
-    setModalContent(content);
-    setIsModalOpen(true);
+  const handleShowMore = (key) => {
+    setShowMore({ ...showMore, [key]: !showMore[key] });
   };
 
-  const closeModal = () => {
-    setModalContent(null);
-    setIsModalOpen(false);
+  const handleInfoClick = (info) => {
+    setActiveInfo(activeInfo === info ? null : info);
   };
 
   const formatValue = (value) => {
@@ -38,86 +41,114 @@ const Timeline = ({ timelineData }) => {
     return formattedValue;
   };
 
+  const truncateText = (text, limit) => {
+    if (text.length <= limit) {
+      return text;
+    }
+    const truncatedText = text.slice(0, limit) + '...';
+    return truncatedText;
+  };
+
+  const renderInfo = (infoText, key) => {
+    const truncatedText = truncateText(infoText, 75);
+
+    if (isMobile) {
+      return (
+        <button className="info" onClick={() => openModal(infoText)}>
+          Voir +
+        </button>
+      );
+    }
+
+    if (infoText.length <= 75) {
+      return <div className="info">{infoText}</div>;
+    }
+
+    return (
+      <div className="info">
+        {showMore[key] ? infoText : truncatedText}
+        <span className="see-more" onClick={() => handleShowMore(key)}>
+          {showMore[key] ? 'Voir +' : 'Voir -'}
+        </span>
+      </div>
+    );
+  };
+
+  const openModal = (content) => {
+    setModalContent(content);
+    setModalIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalContent(null);
+    setModalIsOpen(false);
+  };
+
   return (
-    <div className="timeline-container">
-      <h3>Cliquez sur les boîtes pour voyager dans le temps</h3>
-      {timelineData.map((era) => (
-        <div key={era.era} className="era">
-          <div
-            className={`era-label ${openEra === era.era ? 'open' : ''}`}
-            onClick={() => setOpenEra(openEra === era.era ? null : era.era)}
-          >
-            {era.era}
+    <div>
+      <div className="timeline-container">
+        <VerticalTimeline className="timeline">
+          {timelineData.map((era) => (
+            <VerticalTimelineElement key={era.era} className="era">
+              <div
+                className={`era-label ${openEra === era.era ? 'open' : ''}`}
+                onClick={() => setOpenEra(openEra === era.era ? null : era.era)}
+              >
+                {era.era}
 
-            <div className="era-start">{formatValue(era.eraStart)}</div>
-            <div className="era-end">{formatValue(era.eraEnd)}</div>
+                <div className="era-start">{formatValue(era.eraStart)}</div>
+                <div className="era-end">{formatValue(era.eraEnd)}</div>
 
-            {era.eraInfo && (
-              <button type="button" className="info-btn" onClick={() => handleInfoClick(era.eraInfo)}>
-                ℹ️
-              </button>
-            )}
-          </div>
-
-          {openEra === era.era &&
-            era.periods.map((period) => (
-              <div key={period.name} className="period">
-                <div
-                  className={`period-label ${openPeriod === period.name ? 'open' : ''}`}
-                  onClick={() => setOpenPeriod(openPeriod === period.name ? null : period.name)}
-                >
-                  {period.name}
-                  <div className="period-start">{formatValue(period.periodStart)}</div>
-                  <div className="period-end">{formatValue(period.periodEnd)}</div>
-                  {period.periodInfo && (
-                    <button type="button" className="info-btn" onClick={() => handleInfoClick(period.periodInfo)}>
-                      ℹ️
-                    </button>
-                  )}{' '}
-                </div>
-
-                {openPeriod === period.name &&
-                  period.epochs.map((epoch) => (
-                    <div key={epoch.name} className="epoch">
-                      <div
-                        className={`epoch-label ${openEpoch === epoch.name ? 'open' : ''}`}
-                        onClick={() => setOpenEpoch(openEpoch === epoch.name ? null : epoch.name)}
-                      >
-                        {epoch.name}
-                        <div className="epoch-start">{formatValue(epoch.epochStart)}</div>
-                        <div className="epoch-end">{formatValue(epoch.epochEnd)}</div>
-                        {epoch.epochInfo && (
-                          <button type="button" className="info-btn" onClick={() => handleInfoClick(epoch.epochInfo)}>
-                            ℹ️
-                          </button>
-                        )}{' '}
-                      </div>
-
-                      {openEpoch === epoch.name &&
-                        epoch.stage &&
-                        epoch.stage.map((stage) => (
-                          <div key={stage.name} className="stage">
-                            <div className="stage-label">{stage.name}</div>
-                            <div className="stage-start">{formatValue(stage.stageStart)}</div>
-                            <div className="stage-end">{formatValue(stage.stageEnd)}</div>
-                            {stage.stageInfo && (
-                              <button
-                                type="button"
-                                className="info-btn"
-                                onClick={() => handleInfoClick(stage.stageInfo)}
-                              >
-                                ℹ️
-                              </button>
-                            )}
-                          </div>
-                        ))}
-                    </div>
-                  ))}
+                <div className="info">{renderInfo(era.eraInfo, era.era)}</div>
               </div>
-            ))}
-        </div>
-      ))}
-      <ModalTimeline isOpen={isModalOpen} closeModal={closeModal} content={modalContent} />
+
+              {openEra === era.era &&
+                era.periods.map((period) => (
+                  <VerticalTimelineElement key={period.name} className="period">
+                    <div
+                      className={`period-label ${openPeriod === period.name ? 'open' : ''}`}
+                      onClick={() => setOpenPeriod(openPeriod === period.name ? null : period.name)}
+                    >
+                      {period.name}
+                      <div className="period-start">{formatValue(period.periodStart)}</div>
+                      <div className="period-end">{formatValue(period.periodEnd)}</div>
+                      <div className="info">{renderInfo(period.periodInfo, period.name)}</div>
+                    </div>
+
+                    {openPeriod === period.name &&
+                      period.epochs.map((epoch) => (
+                        <VerticalTimelineElement key={epoch.name} className="epoch">
+                          <div
+                            className={`epoch-label ${openEpoch === epoch.name ? 'open' : ''}`}
+                            onClick={() => setOpenEpoch(openEpoch === epoch.name ? null : epoch.name)}
+                          >
+                            {epoch.name}
+                            <div className="epoch-start">{formatValue(epoch.epochStart)}</div>
+                            <div className="epoch-end">{formatValue(epoch.epochEnd)}</div>
+                            <div className="info">{renderInfo(epoch.epochInfo, epoch.name)}</div>
+                          </div>
+
+                          {openEpoch === epoch.name &&
+                            epoch.stage &&
+                            epoch.stage.map((stage) => (
+                              <VerticalTimelineElement key={stage.name} className="stage">
+                                <div className="stage-label">{stage.name}</div>
+                                <div className="stage-start">{formatValue(stage.stageStart)}</div>
+                                <div className="stage-end">{formatValue(stage.stageEnd)}</div>
+                                <div className="info">{renderInfo(stage.stageInfo, stage.name)}</div>
+                              </VerticalTimelineElement>
+                            ))}
+                        </VerticalTimelineElement>
+                      ))}
+                  </VerticalTimelineElement>
+                ))}
+            </VerticalTimelineElement>
+          ))}
+        </VerticalTimeline>
+      </div>
+      <Modal isOpen={modalIsOpen} onRequestClose={closeModal}>
+        {modalContent}
+      </Modal>
     </div>
   );
 };
