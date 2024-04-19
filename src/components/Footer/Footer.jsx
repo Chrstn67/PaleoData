fed9125c6eb348dad7cd774ffb6bbcf9b086605f
Chrstn67 <chrstn.hmbrt67.com>
2024-04-19 16:25:27 +0200

    MAJ footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -10,20 +10,22 @@ const Footer = () => {
     <footer className="footer">
       <section className="footer-content">
         <section className="footer-section">
-          <h3 className="footer-title">Contactez-nous</h3>
-          <Link to="https://discord.gg/fT97dr49Tv" target="_blank" rel="noopener noreferrer" title="Discord">
-            <FaDiscord className="footer-discord-icon" />
-          </Link>{' '}
-          <Link href="mailto:paleodata@outlook.com" target="_blank" rel="noopener noreferrer" title="Mail">
-            <FaEnvelope className="footer-email-icon" />
-          </Link>
+          <h3 className="footer-title">Viens discuter !</h3>
+          <div>
+            <Link to="https://discord.gg/fT97dr49Tv" target="_blank" rel="noopener noreferrer" title="Discord">
+              <FaDiscord className="footer-discord-icon" />
+            </Link>{' '}
+            <Link href="mailto:paleodata@outlook.com" target="_blank" rel="noopener noreferrer" title="Mail">
+              <FaEnvelope className="footer-email-icon" />
+            </Link>
+          </div>
         </section>
         <section className="footer-section">
           <h3 className="footer-title">À propos</h3>
           <p className="footer-text">
-            PaleoData est votre porte d'entrée dans le monde de la vie ancienne. Notre mission est de fournir un accès
-            aux données et aux informations paléontologiques aux données et informations paléontologiques de manière
-            ouverte et conviviale.
+            PaleoData est ta porte d'entrée dans le monde de la vie ancienne. Notre mission est de fournir un accès aux
+            données et aux informations paléontologiques aux données et informations paléontologiques de manière ouverte
+            et conviviale.
           </p>
         </section>
         <section className="footer-section">
