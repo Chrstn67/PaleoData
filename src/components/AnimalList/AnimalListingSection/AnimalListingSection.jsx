82b36bcc5ebb384adab15674141d153ce46f3c4c
Chrstn67 <chrstn.hmbrt67.com>
2024-04-19 10:17:13 +0200

    refonte page Animal List

--- src/components/AnimalList/AnimalListingSection/AnimalListingSection.jsx
+++ src/components/AnimalList/AnimalListingSection/AnimalListingSection.jsx
@@ -21,42 +21,36 @@ const AnimalCard = ({ animal }) => {
     cardRef.current.style.transform = 'perspective(500px) rotateX(0deg) rotateY(0deg)';
   };
 
-  const shareLink = async (animal) => {
-    if (navigator.share) {
-      try {
-        await navigator.share({
-          title: `Découvre des informations sur ${animal.nom}.`,
-          text: `Découvre des informations sur ${animal.nom}.`,
-          url: `${window.location.origin}/PaleoData/#/animal/${encodeURIComponent(animal.nom)}`,
-        });
-      } catch (error) {
-        console.error('Erreur lors du partage :', error);
-      }
-    }
-  };
+  // const shareLink = async (animal) => {
+  //   if (navigator.share) {
+  //     try {
+  //       await navigator.share({
+  //         title: `Découvre des informations sur ${animal.nom}.`,
+  //         text: `Découvre des informations sur ${animal.nom}.`,
+  //         url: `${window.location.origin}/PaleoData/#/animal/${encodeURIComponent(animal.nom)}`,
+  //       });
+  //     } catch (error) {
+  //       console.error('Erreur lors du partage :', error);
+  //     }
+  //   }
+  // };
 
   return (
     <li ref={cardRef} onMouseMove={handleMouseMove} onMouseLeave={resetCardTransform}>
-      <Link to={`/animal/${encodeURIComponent(animal.nom)}`}>
-        <div
-          style={{
-            background: `center / contain no-repeat url(${animal.image_url})`,
-            height: '17em',
-            width: '17em',
-            borderRadius: '0.5rem',
-            maxWidth: '80%',
-          }}
-        />
-        <h3>{animal.nom}</h3>
-      </Link>
-      <button
+      <div className="image-container">
+        <Link to={`/animal/${encodeURIComponent(animal.nom)}`}>
+          <img src={animal.image_url} alt={animal.nom} />
+          <h3>{animal.nom}</h3>
+        </Link>
+      </div>
+      {/* <button
         type="button"
         onClick={() => {
           shareLink(animal);
         }}
       >
         <BiShareAlt size={20} />
-      </button>
+      </button> */}
     </li>
   );
 };
