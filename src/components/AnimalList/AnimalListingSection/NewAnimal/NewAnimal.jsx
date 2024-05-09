074ee8cf44f8a527c9247b87f7021c3f24d2d32e
Chrstn67 <chrstn.hmbrt67.com>
2024-05-09 10:28:13 +0200

    MAJ TEST NOTIFICATIONS

--- src/components/AnimalList/AnimalListingSection/NewAnimal/NewAnimal.jsx
+++ src/components/AnimalList/AnimalListingSection/NewAnimal/NewAnimal.jsx
@@ -15,60 +15,55 @@ const NewAnimal = ({ animals }) => {
     return <p>De nouveaux animaux seront bientôt mis en ligne</p>;
   }
 
+  // Afficher la notification pour chaque nouvel animal
+  function showNotifications() {
+    newAnimals.forEach((animal) => {
+      const notificationContent = animal.nom;
+      const notificationUrl = `https://chrstn67.github.io/PaleoData/#/animal/${encodeURIComponent(notificationContent)}`;
+
+      // Vérifier si une notification a déjà été affichée pour cet animal
+      const notificationShown = localStorage.getItem(`notificationShown-${animal.nom}`);
+      if (notificationShown) {
+        return;
+      }
+
+      // Définir le drapeau pour cet animal
+      localStorage.setItem(`notificationShown-${animal.nom}`, true);
+
+      const notification = new Notification(notificationContent, {
+        body: 'Un nouvel animal a été ajouté !',
+        icon: 'path/to/icon.png', // Chemin vers l'icône de la notification
+        data: { url: notificationUrl },
+      });
+
+      // Supprimer la notification une fois qu'elle est fermée
+      notification.onclose = (event) => {
+        event.preventDefault(); // empêcher le comportement par défaut
+        notification.close(); // fermer la notification
+      };
+
+      // Rediriger vers l'URL lorsque l'utilisateur clique sur la notification
+      notification.onclick = (event) => {
+        event.preventDefault(); // empêcher le comportement par défaut
+        window.open(notification.data.url, '_blank'); // ouvrir l'URL dans un nouvel onglet
+      };
+    });
+  }
+
   // Demander la permission de l'utilisateur pour afficher les notifications
   function requestPermission() {
     if ('Notification' in window) {
       Notification.requestPermission().then((result) => {
         if (result === 'granted') {
-          showNotification();
+          showNotifications();
         }
       });
     }
   }
 
-  // Afficher la notification
-  function showNotification() {
-    const newAnimalsNames = newAnimals.map((animal) => animal.nom);
-    const notificationContent = newAnimalsNames.join(', ');
-    const notificationUrl = `https://chrstn67.github.io/PaleoData/#/animal/${encodeURIComponent(notificationContent)}`;
-
-    // Vérifier si une notification a déjà été affichée pour ces animaux
-    const notificationShown = localStorage.getItem('notificationShown');
-    if (notificationShown) {
-      const shownAnimals = JSON.parse(notificationShown);
-      const newNotificationAnimals = newAnimalsNames.filter((name) => !shownAnimals.includes(name));
-      if (newNotificationAnimals.length === 0) {
-        return;
-      }
-      // Mettre à jour le drapeau pour les nouveaux animaux
-      localStorage.setItem('notificationShown', JSON.stringify([...shownAnimals, ...newNotificationAnimals]));
-    } else {
-      // Définir le drapeau pour les nouveaux animaux
-      localStorage.setItem('notificationShown', JSON.stringify(newAnimalsNames));
-    }
-
-    const notification = new Notification('Nouveaux animaux ajoutés', {
-      body: notificationContent,
-      icon: 'path/to/icon.png', // Chemin vers l'icône de la notification
-      data: { url: notificationUrl },
-    });
-
-    // Supprimer la notification une fois qu'elle est fermée
-    notification.onclose = (event) => {
-      event.preventDefault(); // empêcher le comportement par défaut
-      notification.close(); // fermer la notification
-    };
-
-    // Rediriger vers l'URL lorsque l'utilisateur clique sur la notification
-    notification.onclick = (event) => {
-      event.preventDefault(); // empêcher le comportement par défaut
-      window.open(notification.data.url, '_blank'); // ouvrir l'URL dans un nouvel onglet
-    };
-  }
-
   // Vérifier si de nouveaux animaux ont été ajoutés et afficher une notification si nécessaire
   if (newAnimals.length > 0 && Notification.permission === 'granted') {
-    showNotification();
+    showNotifications();
   } else if (newAnimals.length > 0 && Notification.permission !== 'denied') {
     requestPermission();
   }
