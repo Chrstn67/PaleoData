a19ab48fa0ea8b7ab1cc62ae988d0e10c32e96cf
Chrstn67 <chrstn.hmbrt67.com>
2024-06-20 14:28:51 +0200

    Ajout de zones d'habitat de certains animaux

--- src/components/AnimalCard/AnimalHabitat/AnimalHabitat.jsx
+++ src/components/AnimalCard/AnimalHabitat/AnimalHabitat.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import PropTypes from 'prop-types';
+import { useState, useEffect } from 'react';
 import { MapContainer, TileLayer, Marker, Popup, Polygon } from 'react-leaflet';
 
 const AnimalHabitat = ({ animal }) => {
@@ -6,26 +7,52 @@ const AnimalHabitat = ({ animal }) => {
     return null;
   }
 
-  const { nom = 'Inconnu', decouverte = {}, habitatCoords = [] } = animal;
+  const { nom = 'Inconnu', decouverte = {}, habitatCoords = [], geologie = {} } = animal;
 
   const defaultPosition = [51.505, -0.09];
-  const discoveryPosition = decouverte.coords || null; // Utilisez null si les coordonnées de découverte ne sont pas définies
+  const discoveryPosition = decouverte.coords || defaultPosition;
+
+  // State pour gérer la position de la carte
+  const [mapCenter, setMapCenter] = useState(discoveryPosition);
+
+  useEffect(() => {
+    setMapCenter(discoveryPosition);
+  }, [discoveryPosition]);
+
+  // Vérification si habitatCoords est un tableau de tableaux
+  const isMultiPolygon = habitatCoords.length > 0 && Array.isArray(habitatCoords[0][0]);
+
+  const renderPolygons = () => {
+    if (isMultiPolygon) {
+      return habitatCoords.map((coords, index) => (
+        <Polygon key={index} positions={coords}>
+          <Popup>Zone d'habitat de {nom}</Popup>
+        </Polygon>
+      ));
+    }
+    return (
+      <Polygon positions={habitatCoords}>
+        <Popup>Zone d'habitat de {nom}</Popup>
+      </Polygon>
+    );
+  };
 
   if (habitatCoords.length === 0) {
     return (
       <div>
         <h2>Zone d'habitat de {nom}</h2>
         <p>Un peu de patience... Des informations arriveront bientôt !!</p>
-        <MapContainer center={defaultPosition} zoom={5} style={{ height: '500px', width: '100%' }}>
+
+        <MapContainer
+          key={JSON.stringify(mapCenter)}
+          center={mapCenter}
+          zoom={5}
+          style={{ height: '500px', width: '100%' }}
+        >
           <TileLayer
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           />
-          {discoveryPosition && (
-            <Marker position={discoveryPosition}>
-              <Popup>Lieu de découverte de {nom}</Popup>
-            </Marker>
-          )}
         </MapContainer>
       </div>
     );
@@ -34,21 +61,44 @@ const AnimalHabitat = ({ animal }) => {
   return (
     <div>
       <h2>Habitat de {nom}</h2>
-      <p> {animal.habitat}</p>
-      <MapContainer center={discoveryPosition || defaultPosition} zoom={5} style={{ height: '500px', width: '100%' }}>
+      <p>{animal.habitat}</p>
+      <h5>
+        Il est important de noter que durant la période {geologie.periode}, notre planète ne ressemblait pas à ce que
+        nous connaissons aujourd&#39;hui...
+      </h5>
+      <MapContainer
+        key={JSON.stringify(mapCenter)}
+        center={mapCenter}
+        zoom={5}
+        style={{ height: '500px', width: '100%' }}
+      >
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         />
         {discoveryPosition && (
           <Marker position={discoveryPosition}>
-            <Popup>Lieu de découverte de {nom}</Popup>
+            <Popup>Lieu de la première découverte de {nom}</Popup>
           </Marker>
         )}
-        {habitatCoords.length > 0 && <Polygon positions={habitatCoords}></Polygon>}
+        {renderPolygons()}
       </MapContainer>
     </div>
   );
 };
 
 export default AnimalHabitat;
+
+AnimalHabitat.propTypes = {
+  animal: PropTypes.shape({
+    nom: PropTypes.string,
+    decouverte: PropTypes.shape({
+      coords: PropTypes.arrayOf(PropTypes.number),
+    }),
+    habitatCoords: PropTypes.arrayOf(PropTypes.arrayOf(PropTypes.arrayOf(PropTypes.number))),
+    geologie: PropTypes.shape({
+      periode: PropTypes.string,
+    }),
+    habitat: PropTypes.string,
+  }).isRequired,
+};
